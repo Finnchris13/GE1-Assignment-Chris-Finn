f8f3397312cabf68b837e0185577cd6dc48326a0
Finnchris13 <finnchris13@gmail.com>
2019-11-21 05:18:23 +0000

    Updating chunk and voxeldata script to make chunk height and width and remove unnessecary vertices for better performance in long run

--- GE1AssignmentChrisFinn/Assets/Scripts/VoxelData.cs
+++ GE1AssignmentChrisFinn/Assets/Scripts/VoxelData.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class VoxelData : MonoBehaviour
 {
 
+    public static readonly int ChunkWidth = 5;
+    public static readonly int ChunkHeight = 15;
+
     public static readonly Vector3[] voxelVerts = new Vector3[8]
     {
 
@@ -19,26 +22,36 @@ public class VoxelData : MonoBehaviour
 
     };
 
-    public static readonly int[,] voxelTris = new int[6,6]
+    public static readonly Vector3[] faceChecks = new Vector3[6]
     {
 
-        {0, 3, 1, 1, 3, 2},
-        {5, 6, 4, 4, 6, 7},
-        {3, 7, 2, 2, 7, 6},
-        {1, 5, 0, 0, 5, 4},
-        {4, 7, 0, 0, 7, 3},
-        {1, 2, 5, 5, 2, 6}
+        new Vector3(0.0f, 0.0f, -1.0f),
+        new Vector3(0.0f, 0.0f, 1.0f),
+        new Vector3(0.0f, 1.0f, 0.0f),
+        new Vector3(0.0f, -1.0f, 0.0f),
+        new Vector3(-1.0f, 0.0f, 0.0f),
+        new Vector3(1.0f, 0.0f, 0.0f),
 
     };
 
-    public static readonly Vector2[] voxelUvs = new Vector2[6]
+    public static readonly int[,] voxelTris = new int[6,4]
+    {
+
+        {0, 3, 1, 2},
+        {5, 6, 4, 7},
+        {3, 7, 2, 6},
+        {1, 5, 0, 4},
+        {4, 7, 0, 3},
+        {1, 2, 5, 6}
+
+    };
+
+    public static readonly Vector2[] voxelUvs = new Vector2[4]
     {
 
         new Vector2(0.0f, 0.0f),
         new Vector2(0.0f, 1.0f),
         new Vector2(1.0f, 0.0f),
-        new Vector2(1.0f, 0.0f),
-        new Vector2(0.0f, 1.0f),
         new Vector2(1.0f, 1.0f)
 
     };
