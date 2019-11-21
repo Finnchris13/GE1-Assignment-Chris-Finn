f39408af5baa15d764eb18c05df0f40d43d78e09
Finnchris13 <finnchris13@gmail.com>
2019-11-21 03:05:47 +0000

    Made a non-voxel terrain.

--- GE1AssignmentChrisFinn/Assets/Scripts/MeshGeneration.cs
+++ GE1AssignmentChrisFinn/Assets/Scripts/MeshGeneration.cs
@@ -11,6 +11,9 @@ public class MeshGeneration : MonoBehaviour
     Vector3[] vertices;
     int[] triangles;
 
+    public int xSize = 20;
+    public int zSize = 20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,36 +22,70 @@ public class MeshGeneration : MonoBehaviour
 
         CreateShape();
         UpdateMesh();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
 
+        
 
     }
 
     void CreateShape()
     {
 
-        vertices = new Vector3[]
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        
+
+        for (int i = 0, z = 0; z <= zSize; z++)
         {
 
-            new Vector3 (0,0,0),
-            new Vector3 (0,0,1),
-            new Vector3 (1,0,0),
-            new Vector3 (1,0,1)
+            for(int x = 0; x <= xSize; x++)
+            {
 
-        };
+                float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
 
-        triangles = new int[]
+                vertices[i] = new Vector3(x, y, z);
+                i++;
+
+            }
+
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z =0; z < zSize; z++)
         {
 
-            0, 1, 2,
-            1, 3, 2
 
-        };
+            for (int x = 0; x < xSize; x++)
+            {
+
+
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+
+
+            }
+            vert++;
+
+
+        }
+
+
 
     }
 
@@ -63,5 +100,22 @@ public class MeshGeneration : MonoBehaviour
         mesh.RecalculateNormals();
 
     }
+
+    /*
+    private void OnDrawGizmos()
+    {
+
+        if (vertices == null)
+            return;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+
+            Gizmos.DrawSphere(vertices[i], .1f);
+
+        }
+
+    }
+    */
 
 }
