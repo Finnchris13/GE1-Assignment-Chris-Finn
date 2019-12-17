cb25f60b3eb6b808b89b53b8e50a555f0e95a7a7
Finnchris13 <finnchris13@gmail.com>
2019-12-17 01:31:23 +0000

    Added some audio visualisation

---
+++ GE1AssignmentChrisFinn/Assets/GoodScripts/InstantiateAudioCubes.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateAudioCubes : MonoBehaviour
+{
+
+    public GameObject _cubePrefab;
+    GameObject[] _sampleCube = new GameObject[512];
+    public float _maxScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        for (int i =0; i < 512; i++)
+        {
+            GameObject _instanceCube = (GameObject)Instantiate(_cubePrefab);
+            _instanceCube.transform.position = this.transform.position;
+            _instanceCube.transform.parent = this.transform;
+            _instanceCube.name = "AudioCube" + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            _instanceCube.transform.position = Vector3.forward * 100;
+            _sampleCube[i] = _instanceCube;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < 512; i++)
+        {
+            if( _sampleCube != null)
+            {
+                _sampleCube[i].transform.localScale = new Vector3(1, (AudioVisScript._samples[i] * _maxScale) + 2, 1);
+            }
+        }
+    }
+}
