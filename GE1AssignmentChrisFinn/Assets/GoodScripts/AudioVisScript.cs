a3f0c7a05ea1e6486fc73128a39897a1d209cfa6
Finnchris13 <finnchris13@gmail.com>
2019-12-17 02:18:40 +0000

    added some new music and a buffer for better audio visualization.

--- GE1AssignmentChrisFinn/Assets/GoodScripts/AudioVisScript.cs
+++ GE1AssignmentChrisFinn/Assets/GoodScripts/AudioVisScript.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof (AudioSource))]
+[RequireComponent(typeof(AudioSource))]
 public class AudioVisScript : MonoBehaviour
 {
     AudioSource myAudio;
     public static float[] _samples = new float[512];
+    public static float[] _freqBands = new float[8];
+    public static float[] _bandBuffer = new float[8];
+
+    float[] _bufferDecrease = new float[8];
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +22,65 @@ public class AudioVisScript : MonoBehaviour
     void Update()
     {
         GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
+    }
+
+    void BandBuffer()
+    {
+
+        for(int g =0; g < 8; g++)
+        {
+            if (_freqBands [g] > _bandBuffer[g])
+            {
+                _bandBuffer[g] = _freqBands[g];
+                _bufferDecrease[g] = 0.005f;
+            }
+
+            if (_freqBands[g] < _bandBuffer[g])
+            {
+                _bandBuffer[g] -= _bufferDecrease[g];
+                _bufferDecrease[g] *= 1.2f;
+            }
+        }
+
     }
 
     void GetSpectrumAudioSource()
     {
+
         myAudio.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+
+    }
+
+    void MakeFrequencyBands()
+    {
+
+        int count = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += _samples[count] * (count + 1);
+                    count++;
+            }
+
+            average /= count;
+
+            _freqBands[i] = average * 10;
+
+        }
+
     }
 
 }
---
+++ GE1AssignmentChrisFinn/Assets/GoodScripts/ParamCube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour
+{
+
+    public int _band;
+    public float _startScale, _scaleMultiplier;
+
+    public bool useBuffer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (useBuffer)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (AudioVisScript._bandBuffer[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+        }
+        else if (!useBuffer)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (AudioVisScript._freqBands[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+        }
+    }
+}
