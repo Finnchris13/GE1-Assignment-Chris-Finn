d8c753a0105c70897b20fe8295eed2880359726b
Finnchris13 <finnchris13@gmail.com>
2019-12-17 01:03:32 +0000

    Created new scene for audio visualisation start. Added some music and started working on audio script.

---
+++ GE1AssignmentChrisFinn/Assets/GoodScripts/AudioVisScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (AudioSource))]
+public class AudioVisScript : MonoBehaviour
+{
+    AudioSource myAudio;
+    public float[] _samples = new float[512];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        myAudio.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+
+}
