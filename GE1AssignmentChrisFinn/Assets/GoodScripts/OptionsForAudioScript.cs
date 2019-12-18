b146f45c6bd5ad2b17e925ea9543a548a440e2bc
Finnchris13 <finnchris13@gmail.com>
2019-12-18 04:58:17 +0000

    Adding particles and UI

---
+++ GE1AssignmentChrisFinn/Assets/GoodScripts/OptionsForAudioScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsForAudioScript : MonoBehaviour
+{
+
+    public GameObject sparks;
+    public Text openMenu;
+    public Image menuBG;
+
+    public bool sparksActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sparks.SetActive(false);
+        openMenu.enabled = false;
+        menuBG.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (sparksActive)
+            sparks.SetActive(true);
+
+        if (!sparksActive)
+            sparks.SetActive(false);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            openMenu.enabled = true;
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+    
+        if(other.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            openMenu.enabled = false;
+            menuBG.enabled = true;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            openMenu.enabled = false;
+            menuBG.enabled = false;
+        }
+
+    }
+
+}
---
+++ GE1AssignmentChrisFinn/Assets/GoodScripts/SparksActivate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SparksActivate : MonoBehaviour
+{
+
+    public bool isActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isActive)
+            gameObject.SetActive(true);
+
+        if (!isActive)
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+}
